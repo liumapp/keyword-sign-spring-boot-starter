13bd23a117ad991ed732245c616b7537c4dd2b79
liumapp <liumapp.com@gmail.com>
2019-09-03 15:44:44 +0800

    add job abstract class for keyword core module

---
+++ keywordsign-core/src/main/java/com/liumapp/keywordsign/core/KeywordSignCore.java
@@ -0,0 +1,17 @@
+package com.liumapp.keywordsign.core;
+
+/**
+ * file KeywordSignCore.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/9/3
+ */
+public class KeywordSignCore
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ keywordsign-core/src/main/java/com/liumapp/keywordsign/core/job/JobDetail.java
@@ -0,0 +1,22 @@
+package com.liumapp.keywordsign.core.job;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * author liumapp
+ * file JobDetail.java
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 6/28/18
+ */
+public abstract class JobDetail<T extends JobData> {
+
+    protected JSONObject jobResult;
+
+    public JobDetail() {
+        this.jobResult = new JSONObject();
+    }
+
+    public abstract JSONObject handle(T data);
+
+}
