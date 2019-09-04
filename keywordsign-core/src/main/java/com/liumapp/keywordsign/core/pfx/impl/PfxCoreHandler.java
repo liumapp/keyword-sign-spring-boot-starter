b0ac04954028b966ba5959665abf336562cbbae5
liumapp <liumapp.com@gmail.com>
2019-09-04 17:04:34 +0800

    add pfx core handler class

---
+++ keywordsign-core/src/main/java/com/liumapp/keywordsign/core/pfx/impl/PfxCoreHandler.java
@@ -0,0 +1,20 @@
+package com.liumapp.keywordsign.core.pfx.impl;
+
+import com.liumapp.keywordsign.core.pfx.PfxHandler;
+
+/**
+ * file PfxCoreHandler.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/9/4
+ */
+public class PfxCoreHandler implements PfxHandler {
+
+    @Override
+    public void writePfxToKeyStore(String keystoreFileName, String keystorePassword, String certAlias, String pfxBase64, String pfxPassword) {
+            
+    }
+
+}
