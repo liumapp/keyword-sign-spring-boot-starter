038215fe311d8b88af0520c38f7481fb48d6e3ce
liumapp <liumapp.com@gmail.com>
2019-09-04 10:56:56 +0800

    add exceptions class

---
+++ keywordsign-core/src/main/java/com/liumapp/keywordsign/core/exceptions/KeyStoreException.java
@@ -0,0 +1,12 @@
+package com.liumapp.keywordsign.core.exceptions;
+
+/**
+ * file KeyStoreException.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/9/4
+ */
+public class KeyStoreException extends RuntimeException {
+}
