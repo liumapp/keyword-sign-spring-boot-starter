7e29da090b716b86d94acd43f202b0381acbd9f6
liumapp <liumapp.com@gmail.com>
2019-09-04 20:20:36 +0800

    update key word core module

--- keywordsign-core/src/main/java/com/liumapp/keywordsign/core/keyword/impl/KeywordCore.java
+++ keywordsign-core/src/main/java/com/liumapp/keywordsign/core/keyword/impl/KeywordCore.java
@@ -1,7 +1,20 @@
 package com.liumapp.keywordsign.core.keyword.impl;
 
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfCanvasProcessor;
+import com.itextpdf.kernel.pdf.canvas.parser.listener.IPdfTextLocation;
+import com.itextpdf.kernel.pdf.canvas.parser.listener.RegexBasedLocationExtractionStrategy;
+import com.liumapp.keywordsign.core.exceptions.KeyStoreException;
 import com.liumapp.keywordsign.core.keyword.Keyword;
+import com.liumapp.qtools.file.base64.Base64FileTool;
+import com.sun.tools.javac.util.BaseFileManager;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
@@ -21,7 +34,32 @@ public class KeywordCore implements Keyword {
 
     @Override
     public List<HashMap<String, Float>> getKeywordPosition(String pdfBase64, String keyword) {
-        return null;
-    }
+        try {
+            PdfReader reader = new PdfReader(Base64FileTool.decodeBase64ToInputStream(pdfBase64));
+            PdfDocument pdfDocument = new PdfDocument(reader);
+            List<HashMap<String, Float>> resultMaps = new ArrayList<>();
+
+            for (int i = 1; i <= pdfDocument.getNumberOfPages(); i++) {
+                PdfPage page = pdfDocument.getPage(i);
+                
+            }
+            List<OverAreaDTO> overAreaDTOS = new ArrayList<OverAreaDTO>();
+
+//            for (String key : keyMap.keySet()) {
+
+                PdfPage page = pdfDocument.getPage(i);
+
+                RegexBasedLocationExtractionStrategy strategy = new RegexBasedLocationExtractionStrategy(keyMap.get(key));
+                PdfCanvasProcessor canvasProcessor = new PdfCanvasProcessor(strategy);
+                canvasProcessor.processPageContent(page);
+                Collection<IPdfTextLocation> resultantLocations = strategy.getResultantLocations();
+                PdfCanvas pdfCanvas = new PdfCanvas(page);
+                pdfCanvas.setLineWidth(0.5f);
+        } catch(Exception e){
+            throw new KeyStoreException("获取关键词位置信息出现异常", e.getCause());
+        }
+
+            return null;
+        }
 
 }
