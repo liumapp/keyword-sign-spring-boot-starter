805819f403ef56eb2581c94b314664e44b963057
liumapp <liumapp.com@gmail.com>
2019-09-05 14:18:20 +0800

    add sign pic impl class

---
+++ keywordsign-core/src/main/java/com/liumapp/keywordsign/core/signpic/impl/SignPicCore.java
@@ -0,0 +1,20 @@
+package com.liumapp.keywordsign.core.signpic.impl;
+
+import com.liumapp.keywordsign.core.signpic.SignPic;
+
+/**
+ * file SignPicCore.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/9/5
+ */
+public class SignPicCore implements SignPic {
+
+    @Override
+    public int[] readWidthAndHeightFromBase64Pic(String picBase64) {
+        return new int[0];
+    }
+
+}
